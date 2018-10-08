82049c9b0cddc5b2e131a2b7f89cf0c93394a5a8
mordochi <lilynnchi@gmail.com>
2018-10-08 18:43:06 +0800

    style: dot animation

--- index.js
+++ index.js
@@ -28,19 +28,25 @@ function submitForm () {
 
   let snackbar = document.getElementById('snackbar');
   if(name && email && text) {
-    let data = new FormData();
-    data.append('token', 'xoxp-397902154451-443441458816-449015804034-fad3b87eaa83e2045e400116ed2c7bb5');
-    data.append('channel', 'CBP9F9X7A');
-    data.append('text', name);
-    data.append('attachments', `[${attachment}]`);
+    let data = {};
+    data.text = "來自官網的新訊息";
+    data.attachments = [{
+      "author_name": name,
+      "color": "#ffe605",
+      "text": email,
+      "fields": [{
+        "title": text,
+        "short": false
+      }]
+    }];
 
     let xhr = new XMLHttpRequest();
-    xhr.open('POST', 'https://slack.com/api/chat.postMessage', true);
+    xhr.open('POST', 'https://hooks.slack.com/services/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', true);
     xhr.onload = function () {
-        // do something to response
-        console.log(this.responseText);
+      // do something to response
+      console.log(this.responseText);
     };
-    xhr.send(data);
+    xhr.send(JSON.stringify(data));
 
     snackbar.innerHTML = '送出囉';
       snackbar.className = "show";
@@ -74,30 +80,77 @@ function submitForm () {
   }
 }
 
-function moveIt(beMoved) {
-  let speed = beMoved.getAttribute('data-scroll-speed');
-  let scrollTop = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
+function moveIt(scrollTop) {
+  let moveItems = document.querySelectorAll('[data-scroll-speed]');
 
-  beMoved.style.setProperty('transform', 'translateY(' + -(scrollTop / speed) + 'px)');
+  for(let i = 0; i < moveItems.length; i++) {
+    let speed = moveItems[i].getAttribute('data-scroll-speed');
+
+    moveItems[i].style.setProperty('transform', 'translateY(' + -(scrollTop / speed) + 'px)');
+  }
 }
 
-function fadeIt(beFaded, position) {
-  let scrollTop = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
-  let elHight = beFaded.clientHeight;
+function fadeIt(scrollTop, farestPosition) {
+  let fadeItems = document.getElementsByClassName('fade');
 
-  //AOS will affect the result
-  if(beFaded.getAttribute('data-aos')) {
-    beFaded.style.setProperty('opacity', 1 - ((scrollTop - position + 150) / elHight));
+  for(let i = 0; i < fadeItems.length; i++) {
+    let bodyRect = document.body.getBoundingClientRect().top;
+    let elRect = fadeItems[i].getBoundingClientRect().top;
+    let itemPosition = elRect - bodyRect;
 
-  } else {
-    beFaded.style.setProperty('opacity', 1 - ((scrollTop - position) / elHight));
+    if(!farestPosition[i] || itemPosition > farestPosition[i]) {
+      farestPosition[i] = itemPosition;
+    }
 
-    if(scrollTop - position > 0) {
-      beFaded.style.setProperty('transform', 'translateY(' + -((scrollTop - position) / elHight) * 50 + 'px)');
+    let elHight = fadeItems[i].clientHeight;
+
+    //AOS will affect the result
+    if(fadeItems[i].getAttribute('data-aos')) {
+      fadeItems[i].style.setProperty('opacity', 1 - ((scrollTop - farestPosition[i] + 150) / elHight));
+
     } else {
-      beFaded.style.setProperty('transform', 'translateY(0px)');
+      fadeItems[i].style.setProperty('opacity', 1 - ((scrollTop - farestPosition[i]) / elHight));
+
+      if(scrollTop - farestPosition[i] > 0) {
+        fadeItems[i].style.setProperty('transform', 'translateY(' + -((scrollTop - farestPosition[i]) / elHight) * 50 + 'px)');
+      } else {
+        fadeItems[i].style.setProperty('transform', 'translateY(0px)');
+      }
     }
   }
+
+
+}
+
+function showDot(scrollTop) {
+  let dots1 = document.querySelectorAll('#dot-pack-1 div');
+  let dots2 = document.querySelectorAll('#dot-pack-2 div');
+  let dots3 = document.querySelectorAll('#dot-pack-3 div');
+
+  for(let i = 0; i < dots1.length; i++) {
+    if(scrollTop > 220 + (15 * i)) {
+      dots1[i].style.setProperty('opacity', '1');
+    } else {
+      dots1[i].style.setProperty('opacity', '0');
+    }
+  }
+
+  for(let i = 0; i < dots2.length; i++) {
+    if(scrollTop > 500 + (15 * i)) {
+      dots2[i].style.setProperty('opacity', '1');
+    } else {
+      dots2[i].style.setProperty('opacity', '0');
+    }
+  }
+
+  for(let i = 0; i < dots3.length; i++) {
+    if(scrollTop > 735 + (15 * i)) {
+      dots3[i].style.setProperty('opacity', '1');
+    } else {
+      dots3[i].style.setProperty('opacity', '0');
+    }
+  }
+
 }
 
 (function() {
@@ -106,23 +159,14 @@ function fadeIt(beFaded, position) {
   let farestPosition = [];
 
   window.addEventListener('scroll', function(){
-    let moveItems = document.querySelectorAll('[data-scroll-speed]');
-    for(let i = 0; i < moveItems.length; i++) {
-      moveIt(moveItems[i]);
-    }
+    let scrollTop = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
 
-    let fadeItems = document.getElementsByClassName('fade');
-    for(let i = 0; i < fadeItems.length; i++) {
-      let bodyRect = document.body.getBoundingClientRect().top;
-      let elRect = fadeItems[i].getBoundingClientRect().top;
-      let itemPosition = elRect - bodyRect;
+    moveIt(scrollTop);
 
-      if(!farestPosition[i] || itemPosition > farestPosition[i]) {
-        farestPosition[i] = itemPosition;
-      }
+    fadeIt(scrollTop, farestPosition);
 
-      fadeIt(fadeItems[i], farestPosition[i]);
-    }
+
+     showDot(scrollTop);
   });
 
 })();
